0fc16ddd1dff56f3f35ad14d02f2c2b3f4069209
Tarie123 <tarymhlang@gmail.com>
2025-07-27 22:30:48 +0200

    done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
+
 from django.views.generic.detail import DetailView
 from .models import Book, Library
+from .models import Library 
+
 
 
 def list_books(request):
