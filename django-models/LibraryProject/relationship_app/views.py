476bf0c401310769d09f8843290d3e31ca60187b
Tarie123 <tarymhlang@gmail.com>
2025-07-27 23:06:27 +0200

    done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book, Library
 from .models import Library 
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 def list_books(request):
@@ -17,3 +19,16 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after successful registration
+            return redirect('login')  # Change to 'home' if you have a home page
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
